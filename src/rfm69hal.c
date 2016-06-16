c7808f1f1233d3e21d19e85bcb1ec1f752c2d070
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2016-06-16 18:55:19 +0200

    Add raspberry pi hal

--- src/rfm69hal.c
+++ src/rfm69hal.c
@@ -1,3 +1,30 @@
 #include "rfm69hal.h"
 
 
+int rfm69hal_init(){
+#ifdef RPI
+    return wiringPiSPISetup(SPI_DEVICE, SPI_SPEED);
+#endif
+}
+
+
+void rfm69hal_delay_ms(uint32_t ms){
+
+
+}
+
+uint32_t rfm69hal_get_timer_ms(){
+
+}
+
+void rfm69hal_enable(bool enable){
+
+}
+
+uint8_t rfm69hal_transfer(uint8_t* bytes, uint16_t size){
+#ifdef RPI
+    wiringPiSPIDataRW(SPI_DEVICE, bytes,size);
+    usleep(5);
+    return byte;
+#endif
+}
--- src/rfm69hal.h
+++ src/rfm69hal.h
@@ -3,39 +3,29 @@
 
 #include <inttypes.h>
 #include <stdbool.h>
+
+#define RPI_
+
+#ifdef RPI
 #include <wiringPiSPI.h>
+#endif
 
 
 #define SPI_SPEED 2000000
 #define SPI_DEVICE 0
 
-void rfm69hal_init(){
-    // Initialize SPI device 0
-    if(wiringPiSPISetup(SPI_DEVICE, SPI_SPEED) < 0) {
-      fprintf(stderr, "Unable to open SPI device\n\r");
-      exit(1);
-    }
+#ifdef __cplusplus
+extern "C" {
+#endif
+        int rfm69hal_init();
+        void rfm69hal_delay_ms(uint32_t ms);
+        uint32_t rfm69hal_get_timer_ms();
+        void rfm69hal_enable(bool enable);
+        uint8_t rfm69hal_transfer(uint8_t* bytes, uint16_t size);
+
+#ifdef __cplusplus
 }
-
-
-void rfm69hal_delay_ms(uint32_t ms){
-
-
-}
-
-uint32_t rfm69hal_get_timer_ms(){
-
-}
-
-void rfm69hal_enable(bool enable){
-
-}
-
-uint8_t rfm69hal_transfer(uint8_t byte){
-    wiringPiSPIDataRW(SPI_DEVICE, byte, 1);
-    return byte;
-}
-
+#endif
 
 
 #endif // RFM69HAL_H
