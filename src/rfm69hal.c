d93dbadb1de7b2f7ae97c2559791163c2df8ef85
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2016-06-19 10:40:21 +0200

    Update rfm69 driver after testing on Rpi

--- src/rfm69hal.c
+++ src/rfm69hal.c
@@ -1,20 +1,37 @@
 #include "rfm69hal.h"
+#include <unistd.h>
+#include <sys/time.h>
+
+uint64_t base;
+
+
+uint64_t get_current_ms(){
+    struct timeval te; 
+    gettimeofday(&te, NULL); // get current time
+    long long milliseconds = te.tv_sec*1000LL + te.tv_usec/1000; // caculate milliseconds
+    // printf("milliseconds: %lld\n", milliseconds);
+    return milliseconds;
+}
+
 
 
 int rfm69hal_init(){
+    base = get_current_ms();
 #ifdef RPI
     return wiringPiSPISetup(SPI_DEVICE, SPI_SPEED);
 #endif
+
 }
 
 
 void rfm69hal_delay_ms(uint32_t ms){
+	usleep(ms*1000);
 
 
 }
 
 uint32_t rfm69hal_get_timer_ms(){
-
+    return get_current_ms()-base;
 }
 
 void rfm69hal_enable(bool enable){
@@ -24,7 +41,5 @@ void rfm69hal_enable(bool enable){
 uint8_t rfm69hal_transfer(uint8_t* bytes, uint16_t size){
 #ifdef RPI
     wiringPiSPIDataRW(SPI_DEVICE, bytes,size);
-    usleep(5);
-    return byte;
 #endif
 }
