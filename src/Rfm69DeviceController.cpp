dc7918b60f868f79cef3835ba1ce82571e2f9451
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2016-08-14 18:22:23 +0200

    Add support for oic-rfm69 bridge

--- src/Rfm69DeviceController.cpp
+++ src/Rfm69DeviceController.cpp
@@ -1,14 +1,31 @@
 #include "Rfm69DeviceController.h"
 #include "QDebug"
+#include "OICDevice.h"
+#include <unistd.h>
+#include "devices/OicBinnarySwitch.h"
 
-Rfm69DeviceController::Rfm69DeviceController(): QObject(0)
+
+
+Rfm69DeviceController::Rfm69DeviceController(SmartHomeServer* parent): QObject((QObject*)parent)
 {
+    m_server = parent;
+
+
+//    m_clientList.append(new OicBinnarySwitch("static Button 1", "00000000-0000-0000-FFFF-0000BD61ADC1"));
+//    m_clientList.append(new OicBinnarySwitch("static Button 2", "0000B960-736F-46F7-BEC0-9E6CBD61ADC2"));
+//    m_clientList.append(new OicBinnarySwitch("static Button 3", "0000B960-736F-46F7-BEC0-9E6CBD61ADC3"));
+//    m_clientList.append(new OicBinnarySwitch("static Button 4", "0000B960-736F-46F7-BEC0-9E6CBD61ADC4"));
+
+
+
 }
 void Rfm69DeviceController::start(){
     pthread_create(&m_thread, NULL, &Rfm69DeviceController::run, this);
 }
 
 void* Rfm69DeviceController::run(void* param){
+    Rfm69DeviceController* ctrl = (Rfm69DeviceController*)param;
+
     uint8_t packet[512];
     uint16_t packet_len;
 
@@ -25,8 +42,48 @@ void* Rfm69DeviceController::run(void* param){
 
         if (packet_len > 0){
             qDebug() << "Received something" << packet_len;
+            QByteArray data((char*)packet, packet_len);
+            QMetaObject::invokeMethod(ctrl, "parseMessage", Q_ARG(QByteArray, data));
         }
+        usleep(1000*100);
+
 
     }
+}
+QString Rfm69DeviceController::createDeviceId(quint32 id){
+    return QString("00000000-0000-0000-FFFF-0000%1").arg(id, 8, 16, QChar('0')).toUpper();
+}
+
+
+OicBaseDevice* Rfm69DeviceController::getDevice(quint32 id){
+    QString uuid = createDeviceId(id);
+
+    for (OicBaseDevice* dev: m_clientList){
+        if (dev->getId() == uuid){
+            return dev;
+        }
+    }
+    return 0;
+}
+
+
+void Rfm69DeviceController::parseMessage(QByteArray message){
+    quint32 deviceId = (message.at(3) << 24) | (message.at(2) << 16) | (message.at(1) << 8) | message.at(0);
+    OcfDeviceType type = (OcfDeviceType)message.at(4);
+    OicBaseDevice* device = getDevice(deviceId);
+
+    if (deviceId == 0){
+        qDebug() << "Create new Rfm69 device" << createDeviceId(deviceId);
+        device = new OicBinnarySwitch("Virtual Button", createDeviceId(deviceId));
+        m_clientList.append(device);
+    }
+
+    if (type == OIC_R_SWITCH_BINARY){
+        quint8 value = message.at(5);
+        qDebug() << deviceId << "value" << value;
+        ((OicBinnarySwitch*)device)->updateValue(value);
+    }
+
+
 }
 
--- src/Rfm69DeviceController.h
+++ src/Rfm69DeviceController.h
@@ -2,13 +2,24 @@
 #define RFM69DEVICECONTROLLER_H
 
 #include <QObject>
+#include "devices/OicBaseDevice.h"
 #include "rfm69.h"
+#include "SmartHomeServer.h"
+
+typedef enum{
+    OIC_R_SWITCH_BINARY
+
+
+}OcfDeviceType;
+
+
+
 
 class Rfm69DeviceController: public QObject
 {
     Q_OBJECT
 public:
-    Rfm69DeviceController();
+    Rfm69DeviceController(SmartHomeServer *parent);
 
 
     void start();
@@ -16,9 +27,16 @@ public:
 signals:
 
 public slots:
+    void parseMessage(QByteArray message);
 
 
 private:
+    QString createDeviceId(quint32 id);
+    OicBaseDevice* getDevice(quint32 id);
+
+    SmartHomeServer* m_server;
+    QList<OicBaseDevice*>  m_clientList;
+    bool isDeviceOnList(QString id);
     pthread_t m_thread;
 };
 
